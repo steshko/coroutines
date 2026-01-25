b4fba239260caabb6533d56ec5d6e3428ac1ca8b
steshko <daniel.steshko@gmail.com>
2026-01-25 13:32:41 -0500

    chore: move dependencies to libs.versions.toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,9 @@
 plugins {
-    val kotlinVersion = "2.3.0"
-    kotlin("jvm") version kotlinVersion apply false
-    kotlin("plugin.spring") version kotlinVersion apply false
-    kotlin("plugin.jpa") version kotlinVersion apply false
-    id("org.springframework.boot") version "4.0.2" apply false
-    id("io.spring.dependency-management") version "1.1.7" apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.spring) apply false
+    alias(libs.plugins.kotlin.jpa) apply false
+    alias(libs.plugins.spring.boot) apply false
+    alias(libs.plugins.spring.dependency.management) apply false
 }
 
 allprojects {
@@ -18,7 +17,7 @@ allprojects {
     plugins.withType<JavaPlugin> {
         configure<JavaPluginExtension> {
             toolchain {
-                languageVersion.set(JavaLanguageVersion.of(25))
+                languageVersion.set(JavaLanguageVersion.of(libs.versions.java.get().toInt()))
             }
         }
     }
